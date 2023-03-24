74f0fcfcde6120e83d85d6f84eebd2c5ce0c8e7d
Luyee Wong <wongluyee1015@hotmail.com>
2023-03-24 23:45:05 +0900

    change user references

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   self.table_name = "sca_messages"
   belongs_to :chatroom, foreign_key: "sca_chatroom_id"
-  belongs_to :user, foreign_key: "sca_user_id"
+  belongs_to :user, foreign_key: "user_id"
 end
